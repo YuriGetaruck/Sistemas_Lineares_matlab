c76da01712c09307e57f10390d6ca321f4008a9a
Yuri Constantino Getaruck <90275932+YuriGetaruck@users.noreply.github.com>
2022-06-10 18:19:51 -0300

    Add files via upload

---
+++ MATLAB/projetolab.m
@@ -0,0 +1,21 @@
+clc;
+
+original = imread('garrafatermicavermelha.jfif');
+%imshow(original)
+
+%imshow(azuis);
+
+vermelhos = (original(:,:,1) > 0) .* (original(:,:,2) < 75) .* (original(:,:,3) < 75); %vermelhos
+amarelos = (original(:,:,1) > 170) .* (original(:,:,2) > 170) .* (original(:,:,3) < 50); %amarelos
+azuis = (original(:,:,1) < 90) .* (original(:,:,2) > 0) .* (original(:,:,3) > 0); %azuis
+verdes = (original(:,:,1) > 170) .* (original(:,:,2) > 170) .* (original(:,:,3) < 50); %amarelos
+
+%pato =  (original(:,:,1) > 230) .* (original(:,:,1) < 255) .* (original(:,:,2) > 165) .* (original(:,:,2) < 250) .* (original(:,:,3) > 55) .* (original(:,:,3) < 70);
+
+orig_double = double(original);
+%imshow(vermelhos);
+%
+vermelhos2 = vermelhos(:,:,ones(1)(:,:));
+apenasgarrafa = orig_double .* vermelhos;
+
+imshow(apenasgarrafa);
